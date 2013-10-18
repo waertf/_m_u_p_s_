1969206d65bb745f75460d45aa5a922b90867e7e
alonso <alonso@wavegis.com.tw>
2013-10-18 17:30:29 +0800

    modify kml application

---
+++ ConsoleApplication1_kml_generator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1_kml_generator
+{
+    class Program
+    {
+        static float first = 125.000000F;
+        static float second = 30.000000F;
+        static float thrid = 0.000000F;
+         const float increate = -0.000100F;
+        static int loop = 60;
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < loop; i++)
+            {
+                Console.WriteLine(first.ToString("0.000000") + "," + second.ToString("0.000000") + "," + thrid.ToString("0.000000"));
+                first += increate;
+                second += increate;
+                
+            }
+        }
+    }
+}
