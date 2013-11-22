9c2051153bcb420706313fc3dd223b293352a467
alonso <alonso@wavegis.com.tw>
2013-11-22 10:00:08 +0800

    add 3 project- asyncTcpClient , asyncTcpServer and LogEveryXMinuteTest

---
+++ ConsoleApplication1LogEveryXMinuteTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Timers;
+using log4net;
+using log4net.Appender;
+using log4net.Config;
+
+namespace ConsoleApplication1LogEveryXMinuteTest
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static System.Timers.Timer aTimer = new System.Timers.Timer(1*1000);
+        static void Main(string[] args)
+        {
+            AutoResetEvent autoEvent = new AutoResetEvent(false);
+            aTimer.Elapsed += new ElapsedEventHandler(aTimer_Elapsed);
+            aTimer.Enabled = true;
+            autoEvent.WaitOne();
+
+        }
+
+        static void aTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            log.Error("test");
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+    
+}
