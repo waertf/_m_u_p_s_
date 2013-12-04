cfb07b9b5bb4df9850c77a1b7157231815189d2e
nb <waertf.register@m2k.com.tw>
2013-12-04 10:42:55 +0800

    implement log every second

--- ConsoleApplication1LogEveryXMinuteTest/Program.cs
+++ ConsoleApplication1LogEveryXMinuteTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Timers;
@@ -17,6 +18,7 @@ namespace ConsoleApplication1LogEveryXMinuteTest
         private static System.Timers.Timer aTimer = new System.Timers.Timer(1*1000);
         static void Main(string[] args)
         {
+            
             AutoResetEvent autoEvent = new AutoResetEvent(false);
             aTimer.Elapsed += new ElapsedEventHandler(aTimer_Elapsed);
             aTimer.Enabled = true;
@@ -26,7 +28,22 @@ namespace ConsoleApplication1LogEveryXMinuteTest
 
         static void aTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            log.Error("test");
+            string outputDirectoryName = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string firstFolder = DateTime.Now.ToString("yyyyMMdd");
+            string secondFolder = DateTime.Now.ToString("HH");
+            string fileName = outputDirectoryName + "\\" + firstFolder + "\\" + secondFolder+"\\"+DateTime.Now.ToString("HHmmss") + ".txt";
+            System.IO.Directory.CreateDirectory(outputDirectoryName + "\\" + firstFolder + "\\" + secondFolder);
+            if (!File.Exists(fileName))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(fileName))
+                {
+                    sw.WriteLine("Hello");
+                    sw.WriteLine("And");
+                    sw.WriteLine("Welcome");
+                }
+            }
+            //log.Error("test");
             Console.WriteLine(DateTime.Now);
         }
     }
