743488fbe211f8692bd15e5e38a1d4f182339a54
nb <waertf.register@m2k.com.tw>
2013-12-18 11:55:50 +0800

    add timer_test project

---
+++ timer_test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace timer_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var autoSendFromSqlTableTimer =
+                    new System.Timers.Timer(5000);
+            autoSendFromSqlTableTimer.Elapsed += (sender, e) => { AutoSend(DateTime.Now); };
+            autoSendFromSqlTableTimer.Enabled = true;
+            Console.ReadLine();
+        }
+
+        static void AutoSend(DateTime dt)
+        {
+            Console.WriteLine(dt);
+        }
+
+    }
+}
