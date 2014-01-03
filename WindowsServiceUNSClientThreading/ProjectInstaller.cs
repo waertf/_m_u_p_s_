7775941dc5a170578b8e982b85c01e769037bd28
nb <waertf.register@m2k.com.tw>
2014-01-03 15:36:54 +0800

    add window service recovery function SetRecoveryOptions

--- WindowsServiceUNSClientThreading/ProjectInstaller.cs
+++ WindowsServiceUNSClientThreading/ProjectInstaller.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration.Install;
+using System.Diagnostics;
 using System.Linq;
 using System.ServiceProcess;
 
@@ -20,10 +21,36 @@ namespace WindowsServiceUNSClientThreading
 
         void ProjectInstaller_AfterInstall(object sender, InstallEventArgs e)
         {
+            SetRecoveryOptions(serviceInstaller1.ServiceName);
+            
             using (ServiceController sc = new ServiceController(serviceInstaller1.ServiceName))
             {
                 sc.Start();
             }
+             
+        }
+        static void SetRecoveryOptions(string serviceName)
+        {
+            int exitCode;
+            using (var process = new Process())
+            {
+                var startInfo = process.StartInfo;
+                startInfo.FileName = "sc";
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+                // tell Windows that the service should restart if it fails
+                startInfo.Arguments = string.Format("failure \"{0}\" reset= 0 actions= restart/60000", serviceName);
+
+                process.Start();
+                process.WaitForExit();
+
+                exitCode = process.ExitCode;
+
+                process.Close();
+            }
+
+            if (exitCode != 0)
+                throw new InvalidOperationException();
         }
     }
 }
