ca9b50194c7f4268ec02e69c7d9f9a2d71ff7b54
nb <waertf.register@m2k.com.tw>
2014-01-03 12:31:23 +0800

    add window service and setup project

---
+++ WindowsServiceUNSClientThreading/Service1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace WindowsServiceUNSClientThreading
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            ProcessStartInfo info = new ProcessStartInfo(@"Client.exe");
+            info.UseShellExecute = false;
+            info.RedirectStandardError = true;
+            info.RedirectStandardInput = true;
+            info.RedirectStandardOutput = true;
+            info.CreateNoWindow = true;
+            info.ErrorDialog = false;
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+
+            Process process = Process.Start(info);
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
