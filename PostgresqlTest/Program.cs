176397670a161997ce6b42ad93dbc47afe51826d
alonso <alonso@wavegis.com.tw>
2014-03-28 14:55:28 +0800

    simulate multi-thread access sql

--- PostgresqlTest/Program.cs
+++ PostgresqlTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace PostgresqlTest
 {
@@ -11,22 +12,11 @@ namespace PostgresqlTest
         {
             try
             {
-                SqlClient sqlClient = new SqlClient(
-                "127.0.0.1",
-                "5432",
-                "postgres",
-                "postgres",
-                "tms2",
-                "true",
-                "0",
-                "20",
-                "0");
-                sqlClient.connect();
-                sqlClient.modify(@"INSERT INTO test(
-            sn, text)
-    VALUES ((select count(sn) from test)::text || 'dd', 'cc');");
-                sqlClient.disconnect();
-                sqlClient.Dispose();
+                for (int i = 0; i < 10; i++)
+                {
+                    Thread sqlThread = new Thread(() => { sqlTest(); });
+                    sqlThread.Start();
+                }
             }
             catch (Exception ex)
             {
@@ -38,6 +28,26 @@ namespace PostgresqlTest
                 Console.ReadLine();
             }
             
+        }
+
+        private static void sqlTest()
+        {
+            SqlClient sqlClient = new SqlClient(
+                "127.0.0.1",
+                "5432",
+                "postgres",
+                "postgres",
+                "tms2",
+                "true",
+                "0",
+                "20",
+                "0");
+            sqlClient.connect();
+            sqlClient.modify(@"INSERT INTO test(
+            sn, text)
+    VALUES ((select count(sn) from test)::text || 'dd', 'cc');");
+            sqlClient.disconnect();
+            sqlClient.Dispose();
         }
     }
 }
