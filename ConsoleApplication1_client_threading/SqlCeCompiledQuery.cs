3d0a8ddfcc8e830284b859fcef61b940105e61c2
alonso <alonso@wavegis.com.tw>
2014-06-19 13:45:59 +0800

    add SqlCeCompiledQuery class

---
+++ ConsoleApplication1_client_threading/SqlCeCompiledQuery.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1_client_threading
+{
+    public static class SqlCeCompiledQuery
+    {
+        /*
+        public static Func<StayCheck, string, IQueryable<string>> searchID1 =
+        CompiledQuery.Compile((StayCheck db, string id) =>
+            from c in db.CheckIfOverTime where c.Uid == id select c.Uid);
+        public static Func<StayCheck, string, IQueryable<string>> searchID2 =
+        CompiledQuery.Compile((StayCheck db, string id) =>
+            from c in db.CheckIfOverTime2 where c.Uid == id select c.Uid);
+        */
+        public static Func<StayCheck, string, IQueryable<string>> SearchID1
+        {
+            get
+            {
+                 Func<StayCheck, string, IQueryable<string>> searchID1 =
+        CompiledQuery.Compile((StayCheck db, string id) =>
+            from c in db.CheckIfOverTime where c.Uid == id select c.Uid);
+                return searchID1;
+            }
+        }
+        public static Func<StayCheck, string, IQueryable<string>> SearchID2
+        {
+            get
+            {
+                Func<StayCheck, string, IQueryable<string>> searchID2 =
+        CompiledQuery.Compile((StayCheck db, string id) =>
+            from c in db.CheckIfOverTime2 where c.Uid == id select c.Uid);
+                return searchID2;
+            }
+        }
+
+    }
+}
