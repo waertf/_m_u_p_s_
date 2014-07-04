3a37085a082e827b97854371e96a82d097f9871b
alonso <alonso@wavegis.com.tw>
2014-07-04 11:00:25 +0800

    add queryForDeviceGroup and two thread to query device status and group

--- WhatsUpSqlClient/Program.cs
+++ WhatsUpSqlClient/Program.cs
@@ -13,23 +13,55 @@ namespace WhatsUpSqlClient
         static void Main(string[] args)
         {
             string connectionString = ConfigurationManager.AppSettings["connectString"];
-            string queryString = @"SELECT DISTINCT dbo_ActiveMonitorStateChangeLog.nPivotActiveMonitorTypeToDeviceID, dbo_ActiveMonitorStateChangeLog.nMonitorStateID, dbo_MonitorState.sStateName
+            string queryStringForDeviceStatus = @"SELECT DISTINCT dbo_ActiveMonitorStateChangeLog.nPivotActiveMonitorTypeToDeviceID, dbo_ActiveMonitorStateChangeLog.nMonitorStateID, dbo_MonitorState.sStateName
 FROM dbo_ActiveMonitorStateChangeLog INNER JOIN dbo_MonitorState ON dbo_ActiveMonitorStateChangeLog.nMonitorStateID = dbo_MonitorState.nMonitorStateID
 WHERE (((dbo_ActiveMonitorStateChangeLog.dEndTime) Is Null) AND ((dbo_ActiveMonitorStateChangeLog.dStartTime) Is Not Null));";
-            while (true)
-            {
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    SqlCommand command = new SqlCommand(queryString, connection);
-                    SqlDataReader reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        Console.WriteLine(String.Format("DeviceID={0}:StateID={1}:StateMsg={2}", reader[0],reader[1],reader[2]));
-                    }
-                }
-                Thread.Sleep(3000);
-            }
+            string queryStringForDeviceGroup = @"SELECT dbo_PivotDeviceToGroup.nDeviceID, dbo_PivotDeviceToGroup.nDeviceGroupID, dbo_DeviceGroup.nParentGroupID, dbo_DeviceGroup.nMonitorStateID
+FROM dbo_PivotDeviceToGroup INNER JOIN dbo_DeviceGroup ON dbo_PivotDeviceToGroup.nDeviceGroupID = dbo_DeviceGroup.nDeviceGroupID;
+";
+            System.Threading.Thread t1 = new System.Threading.Thread
+      (delegate()
+      {
+          while (true)
+          {
+              using (SqlConnection connection = new SqlConnection(connectionString))
+              {
+                  connection.Open();
+                  SqlCommand command = new SqlCommand(queryStringForDeviceStatus, connection);
+                  using (SqlDataReader reader = command.ExecuteReader())
+                  {
+                      while (reader.Read())
+                      {
+                          Console.WriteLine(String.Format("DeviceID={0}:StateID={1}:StateMsg={2}", reader[0], reader[1], reader[2]));
+                      }
+                  }
+              }
+              Thread.Sleep(3000);
+          }
+      });
+            System.Threading.Thread t2 = new System.Threading.Thread
+      (delegate()
+      {
+          while (true)
+          {
+              using (SqlConnection connection = new SqlConnection(connectionString))
+              {
+                  connection.Open();
+                  SqlCommand command = new SqlCommand(queryStringForDeviceGroup, connection);
+                  using (SqlDataReader reader = command.ExecuteReader())
+                  {
+                      while (reader.Read())
+                      {
+                          Console.WriteLine(String.Format("DeviceID={0}:DeviceGroupID={1}:ParentGroupID={2}:MonitorStateID={3}", reader[0], reader[1], reader[2], reader[3]));
+                      }
+                  }
+              }
+              Thread.Sleep(3000);
+          }
+      });
+            t1.Start();
+            t2.Start();
+            
             
         }
     }
