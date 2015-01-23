0573c022b3e891fd697e2559eb4a1d8ba68f22f3
alonso <alonso@wavegis.com.tw>
2015-01-23 16:57:11 +0800

    add Location class

---
+++ ConsoleApplication1_client_threading/Location.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1_client_threading
+{
+    class Location
+    {
+        private double lat { get; set; }
+        private double lon { get; set; }
+
+        public double GetLon()
+        {
+            return lon;
+        }
+
+        public double GetLat()
+        {
+            return lat;
+        }
+
+        public void SetLon(double tmplon)
+        {
+            lon = tmplon;
+        }
+
+        public void SetLat(double tmplat)
+        {
+            lat = tmplat;
+        }
+    }
+}
